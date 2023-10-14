bcfaed2e052d2eeaefc915a99d93ef3bb13d35c4
alex.curtin <alexhcurtin@gmail.com>
2023-10-14 14:13:25 -0400

    add front page hero

---
+++ src/components/guest-hero.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "./ui/button";
+
+const GuestHero = () => {
+	return (
+		<div className="bg-green-300 w-full lg:bg-[url('/hero-image.jpeg')] bg-contain ">
+			<div className="w-full p-32 flex flex-col gap-12 bg-gradient-to-r from-green-300 via-green-300 to-green-200/0">
+				<div>
+					<p className="text-3xl">Welcome bibliophile!</p>
+					<p className="text-3xl">Discover new reading material.</p>
+					<p className="text-3xl">Rate and review your favorite books.</p>
+				</div>
+				<div className="w-fit text-center">
+					<Link href="/signup">
+						<Button
+							variant="secondary"
+							size="lg"
+							className="bg-yellow-300 hover:bg-yellow-400 text-lg font-bold"
+						>
+							Get Started - Create an Account
+						</Button>
+					</Link>
+					<p className="mt-2 text-lg">
+						Already have an account?{" "}
+						<Link href="/signin" className="font-semibold text-slate-800">
+							Sign In
+						</Link>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default GuestHero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { useUser } from "@clerk/nextjs";
-import GuestDash from "~/components/guest-dash";
 
+import GuestDash from "~/components/guest-dash";
 import PageLayout from "~/components/layout";
 import PageHeader from "~/components/ui/page-header";
 import UserDash from "~/components/user-dash";
+import GuestHero from "~/components/guest-hero";
 
 const Home = () => {
 	const { isSignedIn, user } = useUser();
@@ -11,6 +12,7 @@ const Home = () => {
 	return (
 		<PageLayout>
 			<div className="flex flex-col items-center">
+				{!isSignedIn && <GuestHero />}
 				<PageHeader title={`Welcome ${user?.username || "Guest"}!`} />
 				{isSignedIn ? <UserDash /> : <GuestDash />}
 			</div>
