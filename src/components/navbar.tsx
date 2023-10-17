fbdd912355116c1ff2b9f617dc9e7c1d4a7caea0
alex.curtin <alexhcurtin@gmail.com>
2023-10-17 15:56:56 -0400

    redesign search page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,4 @@
-import { useState, type FormEvent, useEffect } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { useUser, SignOutButton } from "@clerk/nextjs";
 import { UserIcon, UserCircle, LogOut, LogIn, UserPlus } from "lucide-react";
 
@@ -13,77 +11,7 @@ import {
 	DropdownMenuItem,
 	DropdownMenuSeparator,
 } from "./ui/dropdown-menu";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import SearchIcon from "./ui/icons/search-icon";
-import { api } from "~/utils/api";
-
-const SearchBar = () => {
-	const [term, setTerm] = useState("");
-	const router = useRouter();
-
-	const { data, refetch } = api.googleApi.previewSearch.useQuery(
-		{ term },
-		{
-			enabled: !!term,
-		},
-	);
-
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			if (term) {
-				refetch();
-			}
-		}, 500);
-
-		return () => {
-			clearTimeout(timer);
-		};
-	}, [term]);
-
-	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		if (term) {
-			router.push(`/search?q=${term}`);
-		}
-	};
-
-	return (
-		<div className="relative group">
-			<form onSubmit={onSubmit} className="flex gap-1 relative">
-				<Input
-					type="text"
-					onChange={(e) => setTerm(e.target.value)}
-					className="md:group-focus-within:w-80 transition-all rounded-full"
-					placeholder="search for books"
-				/>
-				<Button
-					type="submit"
-					variant="ghost"
-					className="text-black/70 hover:bg-transparent absolute right-0 top-0"
-				>
-					<SearchIcon />
-				</Button>
-			</form>
-			{data?.length && (
-				<div className="absolute top-12 left-0 bg-white shadow-lg rounded-sm hidden group-focus-within:block w-full">
-					{data.map((book) => (
-						<Link
-							key={book.googleId}
-							className="border-b text-sm block"
-							href={`/books/${book.googleId}`}
-							onClick={() => setTerm("")}
-						>
-							<p className="hover:bg-neutral-100 p-1 text-black/90">
-								{book.title} - {book.authors[0]?.name}
-							</p>
-						</Link>
-					))}
-				</div>
-			)}
-		</div>
-	);
-};
+import SearchBar from "./search-bar";
 
 const NavBar = () => {
 	const { user } = useUser();
@@ -95,7 +23,7 @@ const NavBar = () => {
 				<h1 className="text-lg font-bold text-black/80">NextBook</h1>
 			</Link>
 			<div className="flex gap-8 items-center">
-				<SearchBar />
+				<SearchBar expand={true} auto={true} />
 				<DropdownMenu>
 					<DropdownMenuTrigger>
 						<Avatar>
